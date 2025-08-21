10f0b01e4b81995e13b5bff3439a97986019fdc1
KaizerStore12 <167608144+KaizerStore12@users.noreply.github.com>
2025-08-22 01:47:22 +0800

    Update and rename server.js to config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+const config = {
+    domain: "https://panel-kamu.com", // ganti dengan domain panel
+    apikey: "PTLA_XXXXXXXXXXXXXXXXXX", // PTLA API Key
+    capikey: "PTLC_XXXXXXXXXXXXXXXXXX", // PTLC API Key
+    egg: "15",
+    nestid: "5",
+    loc: "1"
+};
