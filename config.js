f8c4746286f6c87bfedeedb57a558ffe01194333
KaizerStore12 <167608144+KaizerStore12@users.noreply.github.com>
2025-11-16 23:41:54 +0800

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-const config = {
-    domain: "https://panel-kamu.com", // ganti dengan domain panel
-    apikey: "PTLA_XXXXXXXXXXXXXXXXXX", // PTLA API Key
-    capikey: "PTLC_XXXXXXXXXXXXXXXXXX", // PTLC API Key
-    egg: "15",
-    nestid: "5",
-    loc: "1"
-};
